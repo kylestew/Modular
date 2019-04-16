1fe4013ff74be6ec1b7dda26385803f0ac213491
Kyle Stewart <kylestew@gmail.com>
2019-04-16 15:55:13 -0700

    Pull in files from valley

--- DSPCore/modules/Experimental/Experimental.hpp
+++ DSPCore/modules/Experimental/Experimental.hpp
@@ -3,11 +3,10 @@
 
 #include "Branches.hpp"
 #include "EvenVCO.hpp"
+#include "Plateau.hpp"
 
 namespace library {
     namespace experimental {
-
-//        struct Experimental;
 
         struct Experimental {
             static Pack* init() {
@@ -16,6 +15,7 @@ namespace library {
 
                 p->addModuleBuilder(ModuleBuilder::create<EvenVCO>("EvenVCO"));
                 p->addModuleBuilder(ModuleBuilder::create<Branches>("Branches"));
+                p->addModuleBuilder(ModuleBuilder::create<Plateau>("Plateau"));
 
                 return p;
             }
