d643b702b67aed7d59e608c3bcd69d0670e739c3
Kyle Stewart <kylestew@gmail.com>
2019-04-18 07:55:32 -0700

    Turn clock into gate clock

--- DSPCore/modules/Times/Clock.hpp
+++ DSPCore/modules/Times/Clock.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include "Times.hpp"
-#include "dsp/digital.hpp"
 
 namespace library {
     namespace times {
@@ -35,7 +34,6 @@ namespace library {
             SchmittTrigger resetTrigger;
 
             float phase = 0.0;
-            PulseGenerator gatePulse;
             PulseGenerator resetPulse;
 
             Clock() : Module(NUM_PARAMS, NUM_OPTIONS, NUM_INPUTS, NUM_OUTPUTS, 0, NUM_LABELS, NUM_BUFFERS) {
@@ -44,14 +42,12 @@ namespace library {
                 params[TIME_PARAM].cvIndex = TIME_CV;
 
                 resetPulse.triggerDuration = 10.f;
-                gatePulse.triggerDuration = 10.f;
             }
 
             void reset() override {
                 params[TIME_PARAM].setting = -0.198f;
                 options[RUN_OPTION].value = 1;
 
-                gatePulse.reset();
                 resetPulse.reset();
             }
 
@@ -81,20 +77,15 @@ namespace library {
                     resetClock();
                 }
 
-                bool nextStep = false;
                 float clockTime = powf(2.0, rescale(params[TIME_PARAM].value, -1.f, 1.f, -2.f, 6.f));
                 if (running) {
                     phase += clockTime / engineGetSampleRate();
                     if (phase >= 1.0) {
                         phase -= 1.0;
-                        nextStep = true;
                     }
                 }
-                if (nextStep) {
-                    gatePulse.trigger(1e-3);
-                }
 
-                bool gpulse = running && gatePulse.process(1.0 / engineGetSampleRate());
+                bool gpulse = running && phase < 0.5;
                 bool rpulse = running && resetPulse.process(1.0 / engineGetSampleRate());
 
                 outputs[OUTPUT].value = gpulse ? 1.f : 0.f;
