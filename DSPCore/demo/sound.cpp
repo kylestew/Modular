8a3a981e1a4673a79f4a22bf33af0cd74cb5d80b
Kyle Stewart <kylestew@gmail.com>
2019-05-07 09:18:20 -0700

    Graphing output for development, resolves #117

--- DSPCore/demo/sound.cpp
+++ DSPCore/demo/sound.cpp
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
+//#include <math.h>
 
+//#include "dsp.hpp"
+#include "module_library.hpp"
 #include <sndfile.h>
 
 
-#ifndef		M_PI
-#define		M_PI		3.14159265358979323846264338
-#endif
+//#ifndef		M_PI
+//#define		M_PI		3.14159265358979323846264338
+//#endif
 
 #define		SAMPLE_RATE			44100
 #define		SAMPLE_COUNT		(SAMPLE_RATE * 1)
 #define		AMPLITUDE			(1.0 * 0x7F000000)
-#define		FREQ			    (440.0 / SAMPLE_RATE)
 
 int main() {
     SNDFILE *sf;
     SF_INFO sfinfo;
-    int k;
     int* buffer;
 
     if (! (buffer = (int*)malloc(2 * SAMPLE_COUNT * sizeof(int)))) {
@@ -33,19 +33,30 @@ int main() {
     sfinfo.channels = 1;
     sfinfo.format = (SF_FORMAT_WAV | SF_FORMAT_PCM_24);
 
-    if (! (sf = sf_open ("../sine.wav", SFM_WRITE, &sfinfo))) {
+    if (! (sf = sf_open ("../output.wav", SFM_WRITE, &sfinfo))) {
     	printf ("Error : Not able to open output file.\n") ;
         free (buffer) ;
         return 1 ;
     };
 
 
+    // build module under test
+    library::ModuleBuilder* builder;
+    builder = library::libraryGetModuleBuilder("Experimental", "Sandbox");
+    Module* module = builder->createModule();
+    module->reset();
+    delete builder;
+
+    int outputIdx = 0;
+//    module->params[0].setting = -0.1;
+
 
     // write samples to buffer
-    for (k = 0 ; k < SAMPLE_COUNT ; k++)
-        buffer [k] = AMPLITUDE * sin (FREQ * 2 * k * M_PI) ;
-
-
+    for (int k = 0 ; k < SAMPLE_COUNT ; k++) {
+        module->step();
+        float sample = module->outputs[outputIdx].value;
+        buffer[k] = AMPLITUDE * sample;
+    }
 
     if (sf_write_int (sf, buffer, sfinfo.channels * SAMPLE_COUNT) !=
         sfinfo.channels * SAMPLE_COUNT)
